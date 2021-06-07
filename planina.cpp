0bad5537934d84143ca2fe0b54d0072b4d07ef78
dzuizz <59071340+dzuizz@users.noreply.github.com>
2021-06-07 20:01:50 +0800

    Solved Planina

---
+++ planina.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, ans=1;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        ans *= 2;
+    }
+    ans++;
+    ans *= ans;
+    cout << ans;
+}
